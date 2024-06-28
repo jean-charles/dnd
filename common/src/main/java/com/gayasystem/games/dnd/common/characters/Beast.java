5d80b28b674d8f2783f875bb1e38aeb20e4fad84
Jean-Charles Giardina <561391+jean-charles@users.noreply.github.com>
2024-06-27 22:55:05 -0400

    Update Almiraj stats

--- common/src/main/java/com/gayasystem/games/dnd/common/characters/Beast.java
+++ common/src/main/java/com/gayasystem/games/dnd/common/characters/Beast.java
@@ -1,4 +1,7 @@
 package com.gayasystem.games.dnd.common.characters;
 
+import com.gayasystem.games.dnd.common.dices.Dice;
+
 public class Beast extends Character {
+    protected Dice hitDice;
 }
--- ecosystem/src/main/java/com/gayasystem/games/dnd/ecosystem/beasts/Almiraj.java
+++ ecosystem/src/main/java/com/gayasystem/games/dnd/ecosystem/beasts/Almiraj.java
@@ -1,6 +1,27 @@
 package com.gayasystem.games.dnd.ecosystem.beasts;
 
 import com.gayasystem.games.dnd.common.characters.Beast;
+import com.gayasystem.games.dnd.common.characters.scores.Ability;
+import com.gayasystem.games.dnd.common.characters.scores.AbilityScores;
+import com.gayasystem.games.dnd.common.characters.scores.Skills;
+import com.gayasystem.games.dnd.common.dices.Dice1d6;
 
 public class Almiraj extends Beast {
+    public Almiraj() {
+        abilityScores = new AbilityScores(
+                new Ability(2),
+                new Ability(16),
+                new Ability(10),
+                new Ability(2),
+                new Ability(14),
+                new Ability(10)
+        );
+        abilityScoreIncrease = null;
+        alignment = null;
+        skills = new Skills(0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0);
+        armorClass = 13;
+        equipments = null;
+
+        hitDice = Dice1d6.dice;
+    }
 }
