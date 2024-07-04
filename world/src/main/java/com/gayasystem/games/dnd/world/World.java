96c9d3698df0c6a472f4f287fcc4e4015810d618
Jean-Charles Giardina <561391+jean-charles@users.noreply.github.com>
2024-07-03 21:06:42 -0400

    Add runnable to thing

--- world/src/main/java/com/gayasystem/games/dnd/world/World.java
+++ world/src/main/java/com/gayasystem/games/dnd/world/World.java
@@ -5,6 +5,13 @@ import com.gayasystem.games.dnd.common.Thing;
 import java.util.HashMap;
 import java.util.Map;
 
-public class World {
+public class World implements Runnable {
     private Map<Thing, Coordinate> things = new HashMap<>();
+
+    @Override
+    public void run() {
+        for (var thing : things.keySet()) {
+            thing.run();
+        }
+    }
 }
