e55b87883fe65353cabc019bd941d82173c8c9b1
Jean-Charles Giardina <561391+jean-charles@users.noreply.github.com>
2024-08-11 12:33:58 -0400

    Add carrot image

--- src/main/java/com/gayasystem/games/dnd/drawables/food/DrawableCarrot.java
+++ src/main/java/com/gayasystem/games/dnd/drawables/food/DrawableCarrot.java
@@ -4,30 +4,25 @@ import com.gayasystem.games.dnd.drawables.Drawable;
 import com.gayasystem.games.dnd.world.InGameObject;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
-
-import static java.awt.Color.green;
-import static java.awt.Color.orange;
+import java.io.IOException;
 
 @Service
 public class DrawableCarrot implements Drawable {
+    private final Image image;
+
+    public DrawableCarrot() throws IOException {
+        var img = ImageIO.read(this.getClass().getResource("/images/food/Carrot.png"));
+        image = img.getScaledInstance(40, 60, Image.SCALE_SMOOTH);
+    }
+
     @Override
     public void draw(int width, int height, InGameObject obj, Point point, Graphics2D g) {
         var orientation = obj.orientation();
         int x = point.x;
         int y = point.y;
 
-        g.rotate(orientation.phi().doubleValue());
-        g.setColor(orange);
-        var xs = new int[]{x - 10, x, x + 10};
-        var ys = new int[]{y - 10, y + 20, y - 10};
-        Polygon p = new Polygon(xs, ys, xs.length);
-        g.drawPolygon(p);
-        g.fillPolygon(p);
-
-        g.setColor(green);
-        g.drawLine(x, y - 11, x - 10, y - 27);
-        g.drawLine(x, y - 11, x, y - 27);
-        g.drawLine(x, y - 11, x + 10, y - 27);
+        g.drawImage(image, x - image.getWidth(null), y - image.getHeight(null), null);
     }
 }
